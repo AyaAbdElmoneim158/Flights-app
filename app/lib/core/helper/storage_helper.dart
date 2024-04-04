fe89e8448a0c6855d96b1e84ffb186223c8d9ae2
AyaAbdElmoneim158 <ayaabdelmon@gmail.com>
2024-04-04 11:27:59 +0200

    Add App helpers

---
+++ app/lib/core/helper/storage_helper.dart
@@ -0,0 +1,24 @@
+import 'package:get_storage/get_storage.dart';
+
+class AppLocalStorage {
+  static final AppLocalStorage _instance = AppLocalStorage._internal();
+  factory AppLocalStorage() {
+    return _instance;
+  }
+
+  AppLocalStorage._internal();
+
+  final _storage = GetStorage();
+
+  Future<void> saveData<T>(String key, T value) async {
+    await _storage.write(key, value);
+  }
+
+  Future<void> removeData(String key) async {
+    await _storage.remove(key);
+  }
+
+  Future<void> clearAll() async {
+    await _storage.erase();
+  }
+}
